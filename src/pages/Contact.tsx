2756aa0a96b69a9dfde916b8ba08fc1e68a7ad6b
Chloe Yip <chloe.yip@improving.com>
2024-12-16 12:39:38 -0800

    Prepare for GitHub Pages deployment

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -17,8 +17,8 @@ const Contact = () => {
             <strong>Links:</strong> <br />
             - <a href="https://drive.google.com/file/d/1pBtt-IRdro4QStNrQbE56FuFslQM3u9g/view?usp=drive_link" target="_blank" rel="noopener noreferrer">Epic new Software Engineering Resume</a> <br />
             - <a href="https://ontariospca.ca/blog/pet-health-corner-common-reasons-why-cats-meow-at-their-owners/" target="_blank" rel="noopener noreferrer">Epic new PM Resume</a> <br />
-            - <a href="https://github.com/cyip10" target="_blank" rel="noopener noreferrer">GitHub (Improving and Personal)</a> <br />
-            - <a href="https://www.linkedin.com/in/chloeyip/" target="_blank" rel="noopener noreferrer">LinkedIn</a> <br />
+            - <a href="https://github.com/papaya830" target="_blank" rel="noopener noreferrer">GitHub</a> <br />
+            - <a href="https://www.linkedin.com/in/chloe-yip830/" target="_blank" rel="noopener noreferrer">LinkedIn</a> <br />
           </div>
         </div>
       </main>
