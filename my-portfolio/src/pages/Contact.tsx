4b73fd2db860d31299e1d2df421784dfc4bbb46b
Chloe <chloecocoyip@gmail.com>
2025-05-26 18:54:32 -0700

    update links

--- my-portfolio/src/pages/Contact.tsx
+++ my-portfolio/src/pages/Contact.tsx
@@ -9,7 +9,7 @@ const Contact = () => {
             <a href="mailto:chloecocoyip@gmail.com"> chloecocoyip@gmail.com</a> <br /><br />
 
             <strong>Links:</strong> <br />
-            - <a href="https://drive.google.com/file/d/1pBtt-IRdro4QStNrQbE56FuFslQM3u9g/view?usp=drive_link" target="_blank" rel="noopener noreferrer">Epic new Software Engineering Resume</a> <br />
+            - <a href="https://drive.google.com/drive/u/0/folders/1moOIsxQ4VQhvMhSNczqZmE3Zh8cGPA9O" target="_blank" rel="noopener noreferrer">Epic new Software Engineering Resume</a> <br />
             - <a href="https://github.com/papaya830" target="_blank" rel="noopener noreferrer">GitHub</a> <br />
             - <a href="https://www.linkedin.com/in/chloe-yip830/" target="_blank" rel="noopener noreferrer">LinkedIn</a> <br />
           </div>
