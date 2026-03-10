c184d91d555238fa1fb17eaae2781fecf0706066
Chloe <chloecocoyip@gmail.com>
2026-03-09 22:58:28 -0700

    updated doa content

--- src/pages/experience/Leadership.tsx
+++ src/pages/experience/Leadership.tsx
@@ -4,16 +4,55 @@ const Leadership = () => (
   <div className="content-section">
     <h2 className="section-title">Leadership & Volunteering</h2>
     <div className="section-content">
-
-      <h3 className="job-title">Director of Activities</h3>
+      <h3 className="job-title">Executive Member</h3>
       <p className="company">Software Systems Student Society</p>
       <p className="date">October 2022 - January 2025</p>
+      <p>
+        I was a part of the Software Systems Student Society (SSSS) for 22 months. I joined the executive team in
+        Fall 2022 as a First-Year Representative. Some of the tasks I did during this term included: logistics
+        planning member for the 2023 RootHacks SystemsHack hackathon, organizing first year oriented events,
+        launching the first Girlies Events, and creating a Discord server for the 2022 Fall SoSy cohort.
+      </p>
+      <p>
+        From May 2023 to April 2024, I served as Director of Activities and the Events Committee lead. During my
+        term, I hosted around 25 events for the student society. I would frequently collaborate with other student
+        societies on my campus including the Mechatronics Systems Engineering Student Society and the Sustainable
+        Engineering Student Society. I ran everything from simple events such as Pizza and Board Game Night,
+        starting the first Pancake Breakfast Event for the Surrey Faculty of Applied Sciences (FAS), and the first
+        social event for the women of the FAS at the Surrey campus.
+      </p>
+      <p>
+        As the Events Committee lead, I held monthly meetings and open forums to allow non executive members a
+        chance to plan their own events with the support of the executive team.
+      </p>
+      <p>
+        I decided to run for President for the 2024–2025 term, but rejected my position as I wanted to explore my
+        interest in firmware development with SFU Team Phantom SAE.
+      </p>
+      <p>
+        I returned back as a General Executive from December 2024 to January 2025. I primarily worked on finding
+        sponsors for the 2025 DreamsHack SystemsHack hackathon.
+      </p>
+      <p>The main things I learned from my experience as an executive member on the Software Systems Student Society:</p>
       <ul className="experience-list">
-        <li>Lead a team of 10 executive members to organize events and workshops for over 200 students</li>
-        <li>Established partnerships with local tech companies for sponsorships and mentorship opportunities</li>
-        <li>Coordinated networking events connecting students with industry professionals</li>
+        <li>
+          I grew my confidence and decisiveness through my time as Director of Activities. During my first term,
+          I found it hard to give directions to volunteers which resulted in doing more work myself. Overtime, I
+          became more comfortable giving more context and better direction to those around me and felt generally
+          less nervous when directing those I did not know.
+        </li>
+        <li>
+          Preparing and planning events take a lot of time and effort. Being in an events executive position
+          doesn't give much leeway to procrastinate due to the nature depending on people to fill out grants,
+          visual promotions, etc. To have high quality event success, proper preparation needs to take place.
+        </li>
+        <li>
+          To have a good event, you need to be a good host. A big regret I had was not being a better host
+          during my time as the Director of Activities. It was difficult to be friendly when I was stressed
+          trying to execute the logistics on the day of the event. Having a good host mindset was something I
+          was able to adopt overtime with more experience, and when I first started Treehouse, SFU's Socratica node.
+        </li>
       </ul>
-
     </div>
   </div>
 );
